8799f8918c0849edf08b1733a904ca0467f1896d
AshokKoda <a.koda59@gmail.com>
2022-04-04 21:50:49 +0530

    Added:UC3 - Remove avoidable word from the paragraph

--- src/com/bridgelabz/hashtablesandbst/MyHashMap.java
+++ src/com/bridgelabz/hashtablesandbst/MyHashMap.java
@@ -3,27 +3,36 @@ package com.bridgelabz.hashtablesandbst;
 public class MyHashMap<K, V> {
 
 	LinkedList<K> linkedList;
-	
+
 	public MyHashMap() {
-        this.linkedList = new LinkedList<>();
-    }
-	
+		this.linkedList = new LinkedList<>();
+	}
+
 	public V get(K key) {
-        @SuppressWarnings("unchecked")
+		@SuppressWarnings("unchecked")
 		Node<K, V> mapNode = (Node<K, V>) this.linkedList.search(key);
-        if (mapNode == null)
-            return null;
-        return mapNode.getValue();
-    }
-	
+		if (mapNode == null)
+			return null;
+		return mapNode.getValue();
+	}
+
 	public void add(K key, V value) {
-        @SuppressWarnings("unchecked")
+		@SuppressWarnings("unchecked")
 		Node<K, V> mapNode = (Node<K, V>) this.linkedList.search(key);
-        if (mapNode == null) {
-            mapNode = new Node<>(key, value);
-            this.linkedList.append(mapNode);
-        } else {
-            mapNode.setValue(value);
-        }
-    }
+		if (mapNode == null) {
+			mapNode = new Node<>(key, value);
+			this.linkedList.append(mapNode);
+		} else {
+			mapNode.setValue(value);
+		}
+	}
+
+	public V remove(K key) {
+		@SuppressWarnings("unchecked")
+		Node<K, V> mapNode = (Node<K, V>) this.linkedList.delete(key);
+		if (mapNode == null) {
+			System.out.println("Word is not present phrase");
+		}
+		return mapNode.getValue();
+	}
 }
