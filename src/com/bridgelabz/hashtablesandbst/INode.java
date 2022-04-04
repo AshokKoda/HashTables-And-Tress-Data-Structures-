bea73c608ae8610f0b255a01392ec81f4a277381
AshokKoda <a.koda59@gmail.com>
2022-04-04 21:33:58 +0530

    Added:UC1 - Find word frequency

---
+++ src/com/bridgelabz/hashtablesandbst/INode.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.hashtablesandbst;
+
+public interface INode<K> {
+
+	public K getKey();
+
+	public void setKey(K key);
+
+	public INode<K> getNext();
+
+	public void setNext(INode<K> next);
+}
